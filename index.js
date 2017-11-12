1bb129beaa3fb1536815a5c013bbb86e577668e4
Tim Shipman <bricklayertim@yahoo.com>
2017-11-12 15:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-var bestCustomer = 'not bob';
+
 
 function upperCaseCustomerName(){
   return customerName = customerName.toUpperCase()
 }
 
 function setBestCustomer(){
-  return bestCustomer
+   bestCustomer = 'not bob';
 }
